b960f370710a37d55033e09d0559121a3dabc7b1
kryptokinght <minanshu.1998@gmail.com>
2018-01-27 16:18:14 +0530

    replaced the stackOverflow answer code with my own

--- inject.js
+++ inject.js
@@ -1,7 +1,12 @@
 (function () {
+	//Stack Overflow answer code
+    /*var model =  angular.element(document.getElementById('quantity')).data('$ngModelController');
+	model.$setViewValue("200");*/
 
-    var model =  angular.element(document.getElementById('quantity')).data('$ngModelController');
-	model.$setViewValue("200");
+	//my code but still some help taken from stack Overflow
+	var scope = angular.element($('#quantity')).scope();
+	scope.options.quantity = 200;
+	scope.$apply();
 	document.querySelector('button.btn.place').click();
     console.log("Transaction completed!");
 }());
